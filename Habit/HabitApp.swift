a78b06bbfa7d75ea0401a3391b2cf88588025648
Felipe <flpc.dev@gmail.com>
2023-01-06 12:21:18 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Felipe De Castro Curto on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
